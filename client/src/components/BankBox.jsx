daf014faa2079e8fc9a5327889c71713b92f8bc0
User <user@Users-MacBook-Pro-7.local>
2016-02-24 11:14:10 +0000

    initial committ

---
+++ client/src/components/BankBox.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var sampleAccounts = require('../sample.json');
+var Bank = require('../bank/bank.js');
+
+
+var BankBox = React.createClass({
+  getInitialState: function(){
+    return{ accounts:sampleAccounts };
+  },
+
+
+  render:function(){
+    var bank = new Bank;
+
+    for(var account of this.state.accounts){
+      bank.addAccount(account);
+    }
+
+    return(
+      <div>
+        <h1>React BankBox</h1>
+        <h2>Total: {bank.totalCash()} </h2>
+      </div>
+      )
+  }
+
+})
+
+
+
+
+
+
+module.exports = BankBox;
